dd6f1e3b3715f8c2f7626ff2fb75cd76178243ac
alanhakhyeonsong <songs4805@naver.com>
2023-09-24 13:04:09 +0900

    Chore: api module 생성

--- api/src/main/java/me/ramos/api/ApiApplication.java
+++ api/src/main/java/me/ramos/api/ApiApplication.java
@@ -1,13 +1,13 @@
-package me.ramos.multimodule;
+package me.ramos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultimoduleApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultimoduleApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
--- api/src/test/java/me/ramos/api/ApiApplicationTests.java
+++ api/src/test/java/me/ramos/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package me.ramos.multimodule;
+package me.ramos.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MultimoduleApplicationTests {
+class ApiApplicationTests {
 
     @Test
     void contextLoads() {
