e1de9d882ea798ddfffcc5c110739c4830f2afdb
alanhakhyeonsong <songs4805@naver.com>
2023-09-24 14:58:59 +0900

    Feat: bean injection 의존성 테스트
    scanBasePackages의 설정을 각 multi module 패키지 경로를 명시한다.

--- api/src/main/java/me/ramos/api/ApiApplication.java
+++ api/src/main/java/me/ramos/api/ApiApplication.java
@@ -3,7 +3,9 @@ package me.ramos.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = { "me.ramos.api", "me.ramos.commons" }
+)
 public class ApiApplication {
 
     public static void main(String[] args) {
