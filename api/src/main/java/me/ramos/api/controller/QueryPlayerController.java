c502020e77c9837179fa250162ca46899c58beb5
alanhakhyeonsong <songs4805@naver.com>
2023-09-24 22:22:49 +0900

    Feat: query api 임시 추가

---
+++ api/src/main/java/me/ramos/api/controller/QueryPlayerController.java
@@ -0,0 +1,28 @@
+package me.ramos.api.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import me.ramos.commons.domain.model.Player;
+import me.ramos.commons.service.QueryPlayerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/players")
+@RequiredArgsConstructor
+public class QueryPlayerController {
+
+    private final QueryPlayerService queryPlayerService;
+    @GetMapping
+    public List<Player> findAll() {
+        return queryPlayerService.findAll();
+    }
+
+    @GetMapping("/{playerId}")
+    public Player findByPlayerId(@PathVariable Long playerId) {
+        return queryPlayerService.findByPlayerId(playerId);
+    }
+
+}
