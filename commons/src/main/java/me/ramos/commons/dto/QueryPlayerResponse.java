4cb6933b45c8757f9b7b1eeaee7f03815de35b7f
alanhakhyeonsong <songs4805@naver.com>
2023-09-25 22:05:49 +0900

    Feat: player query response dto

---
+++ commons/src/main/java/me/ramos/commons/dto/QueryPlayerResponse.java
@@ -0,0 +1,30 @@
+package me.ramos.commons.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.ramos.commons.domain.model.Player;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QueryPlayerResponse {
+
+    private String name;
+    private Integer backNumber;
+    private String nationality;
+    private String teamName;
+    private String league;
+
+    public static QueryPlayerResponse of(Player player) {
+        return QueryPlayerResponse.builder()
+                .name(player.getName())
+                .backNumber(player.getBackNumber())
+                .nationality(player.getNationality())
+                .teamName(player.getTeam().getName())
+                .league(player.getTeam().getLeague())
+                .build();
+    }
+}
