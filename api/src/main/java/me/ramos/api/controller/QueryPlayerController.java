52cb588737e83d8f18f5af9d7b15966a1b735fc8
alanhakhyeonsong <songs4805@naver.com>
2023-09-25 22:07:00 +0900

    Refactor: API return type 변경 및 query method 추가

--- api/src/main/java/me/ramos/api/controller/QueryPlayerController.java
+++ api/src/main/java/me/ramos/api/controller/QueryPlayerController.java
@@ -2,7 +2,7 @@ package me.ramos.api.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import me.ramos.commons.domain.model.Player;
+import me.ramos.commons.dto.QueryPlayerResponse;
 import me.ramos.commons.service.QueryPlayerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,12 +17,12 @@ public class QueryPlayerController {
     private final QueryPlayerService queryPlayerService;
 
     @GetMapping
-    public List<Player> findAll() {
+    public List<QueryPlayerResponse> findAll() {
         return queryPlayerService.findAll();
     }
 
     @GetMapping("/{playerId}")
-    public Player findByPlayerId(@PathVariable Long playerId) {
+    public QueryPlayerResponse findByPlayerId(@PathVariable Long playerId) {
         return queryPlayerService.findByPlayerId(playerId);
     }
 
--- commons/src/main/java/me/ramos/commons/service/QueryPlayerService.java
+++ commons/src/main/java/me/ramos/commons/service/QueryPlayerService.java
@@ -1,9 +1,11 @@
 package me.ramos.commons.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import me.ramos.commons.domain.model.Player;
 import me.ramos.commons.domain.repository.PlayerRepository;
+import me.ramos.commons.dto.QueryPlayerResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +16,14 @@ public class QueryPlayerService {
 
     private final PlayerRepository playerRepository;
 
-    public Player findByPlayerId(Long playerId) {
-        return playerRepository.findById(playerId).orElseThrow(RuntimeException::new);
+    public QueryPlayerResponse findByPlayerId(Long playerId) {
+        Player player = playerRepository.findById(playerId).orElseThrow(RuntimeException::new);
+        return QueryPlayerResponse.of(player);
     }
 
-    public List<Player> findAll() {
-        return playerRepository.findAll();
+    public List<QueryPlayerResponse> findAll() {
+        return playerRepository.findAllWithTeam().stream()
+                .map(QueryPlayerResponse::of)
+                .collect(Collectors.toList());
     }
 }
