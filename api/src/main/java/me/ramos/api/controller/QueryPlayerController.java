e29368a75c670096f12a4ff4a59e8ba3100ac433
alanhakhyeonsong <songs4805@naver.com>
2023-09-25 20:28:05 +0900

    Refactor: align code

--- api/src/main/java/me/ramos/api/controller/QueryPlayerController.java
+++ api/src/main/java/me/ramos/api/controller/QueryPlayerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QueryPlayerController {
 
     private final QueryPlayerService queryPlayerService;
+
     @GetMapping
     public List<Player> findAll() {
         return queryPlayerService.findAll();
